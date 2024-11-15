f914237c8d2fd5e372dc4fa6296c2347f2c8a8a4
MaheshkumarN31 <mahesh.n@orotron.com>
2024-11-15 15:19:32 +0530

    first commit

---
+++ assets/js/tailwind.config.js
@@ -0,0 +1,18 @@
+tailwind.config={
+    theme:{
+        
+        fontFamily:{
+            "urbanist" :['Urbanist', 'sans-serif'],    
+        },
+        extend:{
+            colors:{
+                primary:'#9f9fa0',
+                secondary:'#464646',
+                third:'#000',
+                fourth:'#003C7A',
+                fifth:'#e6f4ff',
+            },
+        }
+    },
+
+}
